b1ae06b09a690ce29b1db08044085de8e87e72a0
YASICJUNWOO <midterm@naver.com>
2022-12-15 00:43:45 +0900

    key hide

--- config/settings.py
+++ config/settings.py
@@ -20,9 +20,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lz1$up2*d7fnoaw*-x557u&^5bj!9t3h%gkra$vy516yk#7zvi"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ["INSTA_SECRET_KEY"]
+secret_file = os.path.join(BASE_DIR, 'key.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
