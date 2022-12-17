33b6ac8cb17b1c51f76d8ae2bd44a1f5d2ced086
YASICJUNWOO <midterm@naver.com>
2022-12-17 13:32:56 +0900

    Amenities-admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,9 +4,37 @@ from .models import Room, Amenity
 # Register your models here.
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "city",
+        "price",
+        "rooms",
+        "toilets",
+        "pet_friendly",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
 
 
 @admin.register(Amenity)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+    # 읽기 전용
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
